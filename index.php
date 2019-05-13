7da322bf5746487bf2dd6c2558132c2bd3e12cba
Alejandro <jesusalejandronj@outlook.com>
2019-05-12 22:58:54 -0500

    Hasta seccion 11 - Capitulo 197

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
 
 
-require_once "controller/template.controller.php";
+require_once "controllers/template.controller.php";
+require_once "controllers/administradores.controlador.php";
+
+
+require_once "models/administradores.modelo.php";
+
+require_once "models/rutas.php";
+
 $template= new controllerTemplate();
 
 $template->template();
--- views/moduls/products.php
+++
@@ -1,1 +0,0 @@
-<h1>PAGINA DE PRODUCTOS</h1>
